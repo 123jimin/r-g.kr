05528fd2aee823d1b6285079f017dee071f38f53
Jimin Park <pjm950@gmail.com>
2016-06-30 17:06:51 +0900

    Commited uncommited changed

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 
-var app = module.exports = express();
+const bodyParser = require('body-parser');
+
+var app = express();
+var db = null;
+
+// app.use(require('compression'));
+
+app.use("/static", express.static("static"));
+
+app.set("view engine", 'pug');
+app.use(bodyParser.urlencoded({'extended': false}));
+app.use(bodyParser.json());
+// app.use(require('cookie-parser'));
 
 app.get("/", (req, res) => {
-	res.send("Hello, world!");
+	res.render('base');
 });
 
-module.exports = (db, config) =>  {
+module.exports = (database, config) =>  {
+	db = database;
 	app.listen(config.port, () => {
 		console.log(`Listening on port ${config.port}`);
 	});
