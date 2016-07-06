b5a36a3978a45c1594c29f0bc34f9ccb82090813
Jimin Park <pjm950@gmail.com>
2016-07-06 18:47:22 +0900

    Added scss middleware

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const path = require('path');
 
 const bodyParser = require('body-parser');
+const sassMiddleware = require('node-sass-middleware');
 
 var app = express();
 var db = null;
 
 // app.use(require('compression'));
 
-app.use("/static", express.static("static"));
+app.use(sassMiddleware({
+	'prefix': "/static/css",
+	'src': path.join(__dirname, 'static', 'css'),
+	'dest': path.join(__dirname, 'public', 'css'),
+	'outputStyle': 'compressed',
+}));
+
+app.use("/static", express.static("public"));
 
 app.set("view engine", 'pug');
 app.use(bodyParser.urlencoded({'extended': false}));
