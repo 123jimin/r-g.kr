9675e49424f83502ae0f50336258dc0878688326
Jimin Park <pjm950@gmail.com>
2016-07-08 01:46:38 +0900

    Added register page to route

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const path = require('path');
 
+const passport = require('passport');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const sassMiddleware = require('node-sass-middleware');
+
+const expressSession = require('express-session');
 
 var app = express();
 var db = null;
 
-// app.use(require('compression'));
+app.use(require('compression')());
 
 app.use(sassMiddleware({
 	'prefix': "/static/css",
@@ -21,10 +25,15 @@ app.use("/static", express.static("public"));
 app.set("view engine", 'pug');
 app.use(bodyParser.urlencoded({'extended': false}));
 app.use(bodyParser.json());
-// app.use(require('cookie-parser'));
+app.use(cookieParser());
+app.use(expressSession({'secret': "(change this) r-g.kr session secret"}));
 
 app.get("/", (req, res) => {
 	res.render('index');
+});
+
+app.get("/register", (req, res) => {
+	res.render('register');
 });
 
 module.exports = (database, config) =>  {
